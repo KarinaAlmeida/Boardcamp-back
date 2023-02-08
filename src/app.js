98bebdd7261c439515db4982e8f53a9173d990b9
Karina <karinaalmeida523@gmail.com>
2023-02-08 14:29:12 -0300

    conectado ao postgres

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+
+
+dotenv.config();
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Rodando na porta: ${PORT}`));
