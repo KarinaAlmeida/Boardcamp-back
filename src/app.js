9d79cf74426b3a4f363b12485168def61f3bbc2d
Karina <karinaalmeida523@gmail.com>
2023-02-08 17:10:46 -0300

    rota GET jogos feita

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
-
+// import joi from "joi";
+import {db} from "./database/database.connection.js";
 
 dotenv.config();
 
@@ -10,6 +10,17 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
-
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Rodando na porta: ${PORT}`));
+
+
+//ROTA LISTAR DE GAMES
+server.get("/games", async (req,res) => {
+    try{
+    const jogos= await db.query("SELECT * FROM games");
+    res.send(jogos.rows);
+    } catch (err) {
+        console.log(err);
+       
+    }
+})
