627d8a87c8d527a1d88c381061f4943c159abe98
Karina <karinaalmeida523@gmail.com>
2023-02-09 14:53:17 -0300

    rota POST GAMES feita e funcionando

--- src/app.js
+++ src/app.js
@@ -21,6 +21,30 @@ server.get("/games", async (req,res) => {
     res.send(jogos.rows);
     } catch (err) {
         console.log(err);
-       
+        res.sendStatus(500);
+
     }
 })
+
+//ROTA PARA INSERIR JOGO
+server.post("/games", async (req, res) => {
+const {name, image, stockTotal, pricePerDay } = req.body;
+
+try {
+    const jaExiste= await db.query(`SELECT * FROM games WHERE name = $1;`, [name])
+
+    if (jaExiste.rowCount >0) return res.status(409).send("Jogo já cadastrado!")
+
+    await db.query(`INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4);`, [name, image, parseInt(stockTotal), parseInt(pricePerDay)])
+   
+   
+    return res.status(201).send("Jogo adicionado!")
+}catch (error) {
+        res.status(500).send(error.message);
+    }
+
+
+})
+
+
+
