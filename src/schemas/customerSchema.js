7d99aff983ba41fea84a11a0d702a3fb79cb0b4d
Karina <karinaalmeida523@gmail.com>
2023-02-09 18:38:58 -0300

    bug fix cpf

--- src/schemas/customerSchema.js
+++ src/schemas/customerSchema.js
@@ -3,8 +3,8 @@ import joi from "joi";
 export const cadastraCliente = joi.object({
     name: joi.string().required(),
     phone: joi.string().min(10).max(11).required(),
-    cpf: joi.string().length(11).required(),
+    cpf: joi.string().length(11).pattern(/^[0-9]+$/, 'numbers').required(),
     birthday: joi.date().iso().required(),
   });
 
-  
+ 
